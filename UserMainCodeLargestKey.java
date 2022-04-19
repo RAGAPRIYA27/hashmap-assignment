986b37af4f5f0ab5f1ef951ecfcf287eb02c4d53
RAGAPRIYA27 <101916826+RAGAPRIYA27@users.noreply.github.com>
2022-04-19 09:03:48 +0530

    Add files via upload

---
+++ UserMainCodeLargestKey.java
@@ -0,0 +1,27 @@
+package hashmapassignment;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class UserMainCodeLargestKey {
+	public static String sizeOfResultedHashmap(Map<Integer, String> hashmap)
+	 {
+		
+		int maxvalue = 0;
+		String str=" ";
+		Set<Integer>key=hashmap.keySet();
+		for(Integer i : key)
+		{
+		    if(i>maxvalue )
+		    {
+		        maxvalue = i;
+		        str = hashmap.get(i);
+		    }
+		}
+		return str;
+}
+	
+}
+
+
